8197bc0d4d1fdb7c59de85a2c6b31fa33b054c9e
solo-leveler <shinichiizumi@protonmail.ch>
2024-02-11 18:51:51 +0530

    added genre hook

---
+++ src/Hooks/useGenres.ts
@@ -0,0 +1,16 @@
+import useData from "./useData";
+
+
+export interface Genre {
+    id : number,
+    name : string,
+    slug : string,
+    games_count : number,
+    image_background : string
+}
+
+
+
+const useGenres = () => useData<Genre>('/genres');
+
+export default useGenres
