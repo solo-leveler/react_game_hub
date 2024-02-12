1643f06dc17dfd455f82d31a6aef4037f2608a73
solo-leveler <shinichiizumi@protonmail.ch>
2024-02-12 20:17:36 +0530

    added CriticScore file

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,14 @@
+import {Badge} from "@chakra-ui/react"
+
+interface Props {
+    score:number;
+}
+
+const CriticScore = ({score}:Props) => {
+  let color = score > 75 ? 'green' : score >60 ? 'yellow' : ''
+  return (
+    <Badge fontSize='14px' paddingX={2} borderRadius={4} colorScheme={color}>{score} </Badge>
+  )
+}
+
+export default CriticScore
