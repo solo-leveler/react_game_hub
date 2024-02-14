f0faa62a3b455fe0b4c0e3b4896c5b7ab1193b96
solo-leveler <shinichiizumi@protonmail.ch>
2024-02-14 12:02:57 +0530

    added genrelist file

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,30 @@
+import useGenres, { Genre } from '../Hooks/useGenres'
+import {List,ListItem,Image,HStack,Text,Spinner,Button} from '@chakra-ui/react'
+import getCroppedImageUrl from '../services/image-url';
+
+interface Props{
+    onSelectGenre : (genre : Genre) => void
+    selectedGenre : Genre | null
+}
+export const GenreList = ({selectedGenre,onSelectGenre}:Props) => {
+  const {data,isLoading,error} = useGenres();
+
+  if(error) return null
+  if(isLoading) return <Spinner/>
+
+  return (
+    <>
+
+    <List>
+        {data.map(genre => <ListItem key={genre.id} paddingY={2}>
+        <HStack borderRadius={2} boxShadow={genre.id === selectedGenre?.id ? 'outline' : 'null'}>
+            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background) } />
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'> {genre.name} </Button>
+        </HStack>
+        </ListItem>
+        )}
+    </List>
+
+    </>
+  )
+}
